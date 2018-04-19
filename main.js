b293f35463864ca07fc305b2063f482652e08a49
Eugene Dmitriev <eudmitri@microsoft.com>
2018-04-18 18:46:02 -0700

    wip

--- main.js
+++ main.js
@@ -1,19 +1,24 @@
 'use strict';
-const {app, BrowserWindow} = require('electron');
+const { app, BrowserWindow } = require('electron');
 
 const path = require('path')
 const url = require('url')
-const pug = require('electron-pug')({pretty: true}, {});
+const pug = require('electron-pug')({ pretty: true }, {});
+var events = require('events');
+var fs = require('fs');
 
+let opts;
+let mainWindow;
 
-let mainWindow
+var menubar = new events.EventEmitter();
+menubar.app = app;
 
-app.on('browser-window-created',function(e,window) {
+app.on('browser-window-created', function (e, window) {
   window.setMenu(null);
 });
 
 if (app.isReady()) {
-  appReady()
+  createWindow()
 } else {
   app.on('ready', createWindow)
 }
@@ -22,15 +27,75 @@ app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') {
     app.quit()
   }
-})
+});
 
 app.on('activate', function () {
   if (mainWindow === null) {
     createWindow()
   }
-})
+});
 
-function createWindow () {
+menubar.setOption = function (opt, val) {
+  opts[opt] = val;
+}
+
+menubar.getOption = function (opt) {
+  return opts[opt];
+}
+
+function initMenuBar() {
+  if (typeof opts === 'undefined') {
+    opts = { dir: app.getAppPath() };
+  } else if (typeof opts === 'string') {
+    opts = { dir: opts }
+  } else if (!opts.dir) {
+    opts.dir = app.getAppPath();
+  } else if (!(path.isAbsolute(opts.dir))) {
+    opts.dir = path.resolve(opts.dir);
+  }
+  
+  if (!opts.windowPosition) {
+    opts.windowPosition = (process.platform === 'win32') ? 'trayBottomCenter' : 'trayCenter';
+  }
+  if (typeof opts.showDockIcon === 'undefined') {
+    opts.showDockIcon = false;
+  }
+  
+  // set width/height on opts to be usable before the window is created
+  opts.width = opts.width || 800
+  opts.height = opts.height || 500
+  opts.tooltip = opts.tooltip || ''
+  if (app.dock && !opts.showDockIcon) {
+    app.dock.hide();
+  }
+
+  var iconPath = opts.icon || path.join(opts.dir, 'IconTemplate.png');
+  var cachedBounds // cachedBounds are needed for double-clicked event
+  var defaultClickEvent = opts.showOnRightClick ? 'right-click' : 'click';
+
+  menubar.tray = opts.tray || new Tray(iconPath);
+  menubar.tray.on(defaultClickEvent, clicked);
+  menubar.tray.on('double-click', clicked);
+  menubar.tray.setToolTip(opts.tooltip);
+
+  var supportsTrayHighlightState = false
+  try {
+    menubar.tray.setHighlightMode('never');
+    supportsTrayHighlightState = true;
+  } catch (e) {
+    console.log(e);
+  }
+
+  function clicked (e, bounds) {
+    if (e.altKey || e.shiftKey || e.ctrlKey || e.metaKey || (mainWindow && mainWindow.isVisible())) {
+      return hideWindow();
+    }
+    cachedBounds = bounds || cachedBounds;
+    showWindow(cachedBounds);
+  }
+}
+
+function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 500,
@@ -44,125 +109,68 @@ function createWindow () {
     pathname: path.join(__dirname, 'index.pug'),
     protocol: 'file:',
     slashes: true
-  }))
-
-  // mainWindow.webContents.openDevTools()
+  }));
 
   mainWindow.on('closed', function () {
     mainWindow = null
-  })
+  });
 }
 
-function appReady () {
-  if (app.dock && !opts.showDockIcon) app.dock.hide()
-
-  var iconPath = opts.icon || path.join(opts.dir, 'IconTemplate.png')
-  if (!fs.existsSync(iconPath)) iconPath = path.join(__dirname, 'example', 'IconTemplate.png') // default cat icon
-
-  var cachedBounds // cachedBounds are needed for double-clicked event
-  var defaultClickEvent = opts.showOnRightClick ? 'right-click' : 'click'
-
-  menubar.tray = opts.tray || new Tray(iconPath)
-  menubar.tray.on(defaultClickEvent, clicked)
-  menubar.tray.on('double-click', clicked)
-  menubar.tray.setToolTip(opts.tooltip)
-
-  var supportsTrayHighlightState = false
-  try {
-    menubar.tray.setHighlightMode('never')
-    supportsTrayHighlightState = true
-  } catch (e) {}
-
-  if (opts.preloadWindow) {
-    createWindow()
+function showWindow (trayPos) {
+  if (supportsTrayHighlightState) {
+    menubar.tray.setHighlightMode('always');
+  }
+  if (!menubar.window) {
+    createWindow();
   }
 
-  menubar.showWindow = showWindow
-  menubar.hideWindow = hideWindow
-  menubar.emit('ready')
+  menubar.emit('show');
 
-  function clicked (e, bounds) {
-    if (e.altKey || e.shiftKey || e.ctrlKey || e.metaKey) return hideWindow()
-    if (menubar.window && menubar.window.isVisible()) return hideWindow()
-    cachedBounds = bounds || cachedBounds
-    showWindow(cachedBounds)
+  if (trayPos && trayPos.x !== 0) {
+    // Cache the bounds
+    cachedBounds = trayPos;
+  } else if (cachedBounds) {
+    // Cached value will be used if showWindow is called without bounds data
+    trayPos = cachedBounds;
+  } else if (menubar.tray.getBounds) {
+    // Get the current tray bounds
+    trayPos = menubar.tray.getBounds();
   }
 
-  function createWindow () {
-    menubar.emit('create-window')
-    var defaults = {
-      show: false,
-      frame: false
-    }
-
-    var winOpts = extend(defaults, opts)
-    menubar.window = new BrowserWindow(winOpts)
-
-    menubar.positioner = new Positioner(menubar.window)
-
-    menubar.window.on('blur', function () {
-      opts.alwaysOnTop ? emitBlur() : hideWindow()
-    })
-
-    if (opts.showOnAllWorkspaces !== false) {
-      menubar.window.setVisibleOnAllWorkspaces(true)
-    }
-
-    menubar.window.on('close', windowClear)
-    menubar.window.loadURL(opts.index)
-    menubar.emit('after-create-window')
+  // Default the window to the right if `trayPos` bounds are undefined or null.
+  var noBoundsPosition = null;
+  if ((trayPos === undefined || trayPos.x === 0) && opts.windowPosition.substr(0, 4) === 'tray') {
+    noBoundsPosition = (process.platform === 'win32') ? 'bottomRight' : 'topRight';
   }
 
-  function showWindow (trayPos) {
-    if (supportsTrayHighlightState) menubar.tray.setHighlightMode('always')
-    if (!menubar.window) {
-      createWindow()
-    }
+  var position = menubar.positioner.calculate(noBoundsPosition || opts.windowPosition, trayPos);
 
-    menubar.emit('show')
+  var x = (opts.x !== undefined) ? opts.x : position.x;
+  var y = (opts.y !== undefined) ? opts.y : position.y;
 
-    if (trayPos && trayPos.x !== 0) {
-      // Cache the bounds
-      cachedBounds = trayPos
-    } else if (cachedBounds) {
-      // Cached value will be used if showWindow is called without bounds data
-      trayPos = cachedBounds
-    } else if (menubar.tray.getBounds) {
-      // Get the current tray bounds
-      trayPos = menubar.tray.getBounds()
-    }
+  mainWindow.setPosition(x, y)
+  mainWindow.show();
+  menubar.emit('after-show');
+  return;
+}
 
-    // Default the window to the right if `trayPos` bounds are undefined or null.
-    var noBoundsPosition = null
-    if ((trayPos === undefined || trayPos.x === 0) && opts.windowPosition.substr(0, 4) === 'tray') {
-      noBoundsPosition = (process.platform === 'win32') ? 'bottomRight' : 'topRight'
-    }
-
-    var position = menubar.positioner.calculate(noBoundsPosition || opts.windowPosition, trayPos)
-
-    var x = (opts.x !== undefined) ? opts.x : position.x
-    var y = (opts.y !== undefined) ? opts.y : position.y
-
-    menubar.window.setPosition(x, y)
-    menubar.window.show()
-    menubar.emit('after-show')
-    return
+function hideWindow () {
+  if (supportsTrayHighlightState) {
+    menubar.tray.setHighlightMode('never');
   }
-
-  function hideWindow () {
-    if (supportsTrayHighlightState) menubar.tray.setHighlightMode('never')
-    if (!menubar.window) return
-    menubar.emit('hide')
-    menubar.window.hide()
-    menubar.emit('after-hide')
+  if (!menubar.window) {
+    return;
   }
+  menubar.emit('hide')
+  mainWindow.hide();
+  menubar.emit('after-hide')
+}
 
-  function windowClear () {
-    delete menubar.window
-    menubar.emit('after-close')
-  }
+function windowClear () {
+  delete menubar.window
+  menubar.emit('after-close')
+}
 
-  function emitBlur () {
-    menubar.emit('focus-lost')
-  }
+function emitBlur () {
+  menubar.emit('focus-lost')
 }
